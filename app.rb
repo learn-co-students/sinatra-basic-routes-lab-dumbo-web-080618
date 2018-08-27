e32551b1d92c73205c5617d05d664efb915296d3
Madeline Celis <celiscb@vcu.edu>
2018-08-27 19:02:34 -0400

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,17 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+    get '/name' do
+      "My name is Madeline"
+    end
+
+    get '/hometown' do
+      "My hometown is Arlington, VA"
+    end
+
+    get '/favorite-song' do
+      "My favorite song is I ran (so far away)"
+    end
+
 end
