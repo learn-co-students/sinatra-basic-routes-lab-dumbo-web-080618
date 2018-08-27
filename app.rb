5ebb73e4bdf393a1193982e3a6038b7a47b0225c
github username <github email address>
2018-08-26 22:14:11 -0400

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,16 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
-end
+
+  # Define your GET '/' route below and respond with "Hello, World!"
+    get '/name' do
+      "My name is Felix Chan"
+    end
+    get '/hometown' do
+      "My hometown is Winston - Salem"
+    end
+    get '/favorite-song' do
+      "My favorite song is Bios"
+    end
+
+  end
